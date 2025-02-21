f3d6722f8cd20578d7350ad75568b85625865bde
Elhan Mahmutovic <elhan.mahmutovic03@gmail.com>
2025-02-21 23:28:09 +0100

    auth for public and private facing sides

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,12 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function Component() {
-  const { data: session } = useSession();
+  const router = useRouter()
 
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn('google')}>Sign in with Google</button>
-    </>
-  );
+  useEffect(() => {
+    router.push('/home')
+  }, [router])
+
+  return null
 }
